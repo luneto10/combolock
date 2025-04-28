6c5d52650b0b54a4b46c50b5d47cc4f33b23c952
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2025-04-28 10:37:00 -0500

    Inittial Comit

---
+++ src/rotary-encoder.c
@@ -0,0 +1,101 @@
+/**************************************************************************//**
+ *
+ * @file rotary-encoder.c
+ *
+ * @author (STUDENTS -- TYPE YOUR NAME HERE)
+ * @author (STUDENTS -- TYPE YOUR NAME HERE)
+ *
+ * @brief Code to determine the direction that a rotary encoder is turning.
+ *
+ ******************************************************************************/
+
+/*
+ * ComboLock GroupLab assignment and starter code (c) 2022-24 Christopher A. Bohn
+ * ComboLock solution (c) the above-named students
+ */
+
+#include <CowPi.h>
+#include "interrupt_support.h"
+#include "rotary-encoder.h"
+
+#define A_WIPER_PIN         (16)
+#define B_WIPER_PIN         (A_WIPER_PIN + 1)
+
+typedef enum {
+    HIGH_HIGH, HIGH_LOW, LOW_LOW, LOW_HIGH, UNKNOWN
+} rotation_state_t;
+
+volatile cowpi_ioport_t *ioport = (cowpi_ioport_t *)(0xD0000000);
+static rotation_state_t volatile state;
+static direction_t volatile direction = STATIONARY;
+static int volatile clockwise_count = 0;
+static int volatile counterclockwise_count = 0;
+
+static void handle_quadrature_interrupt();
+
+void initialize_rotary_encoder() {
+    cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
+    state = HIGH_HIGH; 
+
+    clockwise_count = 0;
+    counterclockwise_count = 0;
+
+    register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
+}
+
+uint8_t get_quadrature() {
+    uint8_t A = (ioport->input >> A_WIPER_PIN) & 0x1;
+    uint8_t B = (ioport->input >> B_WIPER_PIN) & 0x1;
+    return (A << 0) | (B << 1);
+}
+
+char *count_rotations(char *buffer) {
+    sprintf(buffer, "CW:%2d CCW:%2d", clockwise_count, counterclockwise_count);
+    return buffer;
+}
+
+direction_t get_direction() {
+    direction_t direction_copy = direction;
+    direction = STATIONARY;
+    return direction_copy;
+}
+
+static void handle_quadrature_interrupt() {
+    static rotation_state_t last_state = HIGH_HIGH;
+    uint8_t quadrature = get_quadrature();
+
+    switch (quadrature) {
+        case 0b00:
+            if (last_state == HIGH_LOW) {
+                clockwise_count++;
+                direction = CLOCKWISE;
+            }
+            else if (last_state == LOW_HIGH) {
+                counterclockwise_count++;
+                direction = COUNTERCLOCKWISE;
+            }
+            state = LOW_LOW;
+            break;
+
+        case 0b01:
+            if (last_state == HIGH_HIGH || last_state == LOW_LOW) {
+                state = LOW_HIGH;
+            }
+            break;
+
+        case 0b10:
+            if (last_state == HIGH_HIGH || last_state == LOW_LOW) {
+                state = HIGH_LOW;
+            }
+            break;
+
+        case 0b11: 
+            state = HIGH_HIGH;
+            break;
+
+        default:
+            break;
+    }
+
+    last_state = state;
+}
